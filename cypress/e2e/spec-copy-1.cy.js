48214f3a8c97ecd9a423f61fc1f01aca96e0371c
Aoyama Kazuki <sekibun3109@gmail.com>
2023-10-26 16:07:25 +0900

    Loginの実装,Station1のテストを修正

--- cypress/e2e/spec-copy-1.cy.js
+++ cypress/e2e/spec-copy-1.cy.js
@@ -1,28 +1,22 @@
 describe("My First Test", () => {
   it("Correct test", () => {
-    cy.visit("http://localhost:3000"); // または http://localhost:3000 など
-
-    cy.get("#input-email").type("test@test.com"); // ABC と入力する
-    cy.get("#input-button").click(); // ボタンを押す
-    cy.get("#result").should("have.text", "test@test.com"); // 出力をチェックする
-    cy.get("#input-email").should("have.value", "test@test.com"); // input タグの場合
+    cy.visit("http://localhost:3000/login");
+    cy.get("#input-email").type("test@test.com");
+    cy.get("#input-password").type("test");
+    cy.get("#input-button").click();
   });
-
   it("Incorrect test", () => {
-    cy.visit("http://localhost:3000"); // または http://localhost:3000 など
-
-    cy.get("#input-email").type("test"); // ABC と入力する
-    cy.get("#input-button").click(); // ボタンを押す
-    cy.get("#result").should("have.text", "エラー"); // 出力をチェックする
-    cy.get("#input-email").should("have.value", "test"); // input タグの場合
+    cy.visit("http://localhost:3000/login");
+    cy.get("#input-email").type("test");
+    cy.get("#input-password").type("test");
+    cy.get("#input-button").click();
   });
-
   it("Incorrect test", () => {
-    cy.visit("http://localhost:3000"); // または http://localhost:3000 など
-
-    cy.get("#input-email").type("test@test"); // ABC と入力する
-    cy.get("#input-button").click(); // ボタンを押す
-    cy.get("#result").should("have.text", "エラー"); // 出力をチェックする
-    cy.get("#input-email").should("have.value", "test@test"); // input タグの場合
+    cy.visit("http://localhost:3000/login");
+    cy.get("#input-email").type("test");
+    cy.get("#input-password").type(
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    );
+    cy.get("#input-button").click();
   });
 });
