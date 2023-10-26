4a2d5dfaf261d3085c316b75e8355db0982a7be8
Aoyama Kazuki <sekibun3109@gmail.com>
2023-10-26 16:11:15 +0900

    Station2のテストを修正

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,18 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import Header from "./components/Header";
 
 describe("App", () => {
   test("ログイン画面のコンポーネント", () => {
-    render(<App />);
+    render(<Header />);
     const inputEmailLabel = screen.getByLabelText("メールアドレス");
     const inputEmail = screen.getByRole("textbox", { name: "メールアドレス" });
+    const inputPasswordLabel = screen.getByLabelText("パスワード");
+    const inputPassword = screen.getByLabelText("パスワード");
     const inputButton = screen.getByRole("button", { name: "送信" });
-    const result = screen.getByText("未入力");
     expect(inputEmailLabel).toBeInTheDocument();
     expect(inputEmail).toBeInTheDocument();
+    expect(inputPasswordLabel).toBeInTheDocument();
+    expect(inputPassword).toBeInTheDocument();
     expect(inputButton).toBeInTheDocument();
-    expect(result).toBeInTheDocument();
   });
 });
